ec8f8b7fa0a7262707c5b7f49cc0ab6ddddafcb4
beryll1um <beryllium.d3v@gmail.com>
2021-01-13 21:18:02 +0200

    mod(examples): examples modified

--- examples/interfaces/interfaces.cpp
+++ examples/interfaces/interfaces.cpp
@@ -1,24 +1,49 @@
 #include <unified.hpp>
 
+#include <unified/graphics/2d/drawable/vertex_array.hpp>
+
 #include <imgui_layer/imgui_layer.hpp>
 
 using namespace Unified;
+using namespace Unified::Graphics;
 
+class TriangleLayer : public Layer
+{
+public:
+
+    const Application *application;
+
+    Graphics::Vertex2d *triangle;
+
+    Graphics2D::VertexArray vertex_array;
+
+    TriangleLayer(Application *application, Graphics::Vertex2d *triangle) : application(application), triangle(triangle), vertex_array(PrimitiveType::Triangles, 3) { }
+
+    virtual void OnUpdate(Time) override {
+        vertex_array.write(triangle, sizeof(Graphics::Vertex2d[3]));
+        application->draw(vertex_array);
+    }
+
+};
 class ImGuiLayer : public Modules::ImGuiLayer
 {
 public:
 
-    ImGuiLayer(Application *application) {
-        Modules::ImGuiLayer::Create(application);
+    Graphics::Vertex2d *triangle;
+
+    ImGuiLayer(Application *application, Graphics::Vertex2d *triangle) : triangle(triangle) {
+        Create(application);
     }
 
     virtual ~ImGuiLayer() {
-        Modules::ImGuiLayer::Destroy();
+        Destroy();
     }
 
     virtual void OnUpdate(Time) override {
         ImGui::Begin("ExampleInterfaces");
-        ImGui::Text("Hello World!");
+        ImGui::ColorEdit3("LD", (float*)(&triangle[0].color));
+        ImGui::ColorEdit3("CU", (float*)(&triangle[1].color));
+        ImGui::ColorEdit3("RD", (float*)(&triangle[2].color));
         ImGui::End();
     }
 
@@ -28,6 +53,11 @@ class ExampleInterfaces : public Application
 {
 public:
 
+    Graphics::Vertex2d triangle[3] = 
+    { { { -0.8, -0.8 }, { 1.f, 0.f, 0.f, 1.f } },
+      { {  0.0,  0.8 }, { 1.f, 0.f, 0.f, 1.f } },
+      { {  0.8, -0.8 }, { 1.f, 0.f, 0.f, 1.f } } };
+
     void window_resize_event(const WindowResizeEvent &event) {
         set_viewport(event.size);
     }
@@ -35,7 +65,8 @@ public:
 public:
 
     ExampleInterfaces() : Application("ExampleInterfaces") {
-        push_layer<ImGuiLayer>(this);
+        push_layer<TriangleLayer>(this, triangle);
+        push_layer<ImGuiLayer>(this, triangle);
         set_frame_limit(60);
     }
 
