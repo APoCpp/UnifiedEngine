92b2f728013f2f48b903f9e4821c3b27c8fe683f
beryll1um <vitalii.babichuk@gmail.com>
2021-02-19 23:20:57 +0200

    ret(texture): texture example returned

--- src/graphics/render_target.cpp
+++ src/graphics/render_target.cpp
@@ -8,6 +8,9 @@ UNIFIED_GRAPHICS_BEGIN_NAMESPACE
 RenderTarget::RenderTarget() {
     if (!gladLoadGL())
         throw Exceptions::initialization_failed("failed to initialize glad");
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void RenderTarget::clear(const Color &color) {
